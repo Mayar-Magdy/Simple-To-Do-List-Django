12e6aa50aace27bcacd06ec4bcd6c28b98223333
Mayar <maimagdy238@gmail.com>
2024-03-27 08:38:28 +0200

    add edit_task

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def index(request):
@@ -8,7 +8,18 @@ def index(request):
 def add_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
-        Task.objects.create(title=title)
+        description = request.POST.get('description', '')
+        Task.objects.create(title=title, description=description)
+    return redirect('index')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description', '')
+        task.title = title
+        task.description = description
+        task.save()
     return redirect('index')
 
 def complete_task(request, task_id):
